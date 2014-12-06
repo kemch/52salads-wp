70ed7568a1e3110820b6095187d64bfd64b15f33
Kurt Emch <kurt.emch@showclix.com>
2014-12-06 09:52:55 -0500

    adds wp header and footer, some gitignoreedits.

---
+++ footer.php
@@ -0,0 +1,4 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<? bloginfo('template_directory'); ?>/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="<? bloginfo('template_directory'); ?>/js/plugins.js"></script>
+<script src="<? bloginfo('template_directory'); ?>/js/main.js"></script>
--- index.php
+++ index.php
@@ -3,30 +3,28 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>52 Salads</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="<? bloginfo('template_directory'); ?>/css/main.css">
-        <script src="<? bloginfo('template_directory'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <? if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div class="entry">
-                  <? the_content(); ?>
-                </div>
-            <? endwhile; else : ?>
-                <p><? _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-            <? endif; ?>
-        </div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<? bloginfo('template_directory'); ?>/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="<? bloginfo('template_directory'); ?>/js/plugins.js"></script>
-        <script src="<? bloginfo('template_directory'); ?>/js/main.js"></script>
-
-    </body>
+	<head>
+		<? get_header(); ?>
+	</head>
+	<body>
+		<header>
+			<h1>52 Salads</h1>
+		</header>
+		<div class="container">
+			<? if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="entry">
+				  <? the_content(); ?>
+				  <? the_field('recipe'); ?>
+				  <?
+						$image = get_field('image');
+						if( !empty($image) ): ?>
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<?php endif; ?>
+				</div>
+			<? endwhile; else : ?>
+					<p><? _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<? endif; ?>
+		</div>
+		<? get_footer(); ?>
+	</body>
 </html>
