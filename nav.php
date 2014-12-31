8a497b6106318910d9477c9651692f8d8ecaffae
Kurt Emch <kurt.emch@showclix.com>
2014-12-30 21:44:01 -0500

    remove unused links

--- nav.php
+++ nav.php
@@ -9,9 +9,9 @@
 				<i class="fa fa-cutlery"></i>
 			</a>
 		</li>
-		<li><a class="js-nav-salads" href="/">Salads</a></li>
-		<li><a class="js-nav-vinaigrette" href="#">Vinaigrette</a></li>
-		<li><a class="js-nav-season" href="#">In Season</a></li>
-		<li><a class="js-nav-hacks" href="#">Salad Hacks</a></li>
+		<!-- <li><a class="js-nav-salads" href="/">Salads</a></li> -->
+		<!-- <li><a class="js-nav-vinaigrette" href="#">Vinaigrette</a></li> -->
+		<!-- <li><a class="js-nav-season" href="#">In Season</a></li> -->
+		<!-- <li><a class="js-nav-hacks" href="#">Salad Hacks</a></li> -->
 	</ul>
 </nav>
