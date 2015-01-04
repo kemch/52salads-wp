3e21fe2ff8882d66f2843c2c76c3f716af51c903
Kurt Emch <kurt.emch@showclix.com>
2015-01-04 10:33:48 -0500

    js cleanup

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 $(function(){
 
-	$('.js-nav-season').click(function(e){
-
-	});
-
 	$('.js-details').click(function(e){
 		var source = $(this);
 		var target = $($(this).data('target'));
-		// var target = $($(this).data('target'));
 		if (target.is(':visible')) {
-			console.log('visible');
 			target.slideUp(200);
 		} else {
-			console.log('not visible');
 			target.slideDown(200);
 		}
-		console.log(target);
+		e.preventDefault();
 	});
 
 });
