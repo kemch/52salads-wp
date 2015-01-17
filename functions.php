052fccae479a9227e00704295c701bbdf5e8c930
Kurt Emch <kurt.emch@showclix.com>
2015-01-17 14:23:50 -0500

    adds meta into head via wp_head

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ add_filter('single_template', create_function(
 		return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
 		return $the_template;' )
 );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
--- header.php
+++ header.php
@@ -6,14 +6,13 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>52 Salads</title>
-<meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<? bloginfo('template_directory'); ?>/css/main.css">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="<? bloginfo('template_directory'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
 <script src="//use.typekit.net/sek7qjp.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
+
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -23,5 +22,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-57389583-1', 'auto');
 ga('send', 'pageview');
 </script>
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
 </head>
